92e94a89dc8764fb5ea2d64e22e66a260d7b94e1
cpandares <>
2022-06-30 11:36:03 -0400

    routes section 3

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Navigation from './routes/Navigation'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <>
+     <Navigation />
+   </>
+  )
+}
+
+export default App
