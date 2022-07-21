758df51e06af8f7c53ed5ed13145e18b568cdd5d
cpandares <>
2022-07-21 12:50:59 -0400

    init pre npm

--- src/02-components-params/pages/ShoppingPage.tsx
+++ src/02-components-params/pages/ShoppingPage.tsx
@@ -6,7 +6,7 @@ import {
 } from '../components/';
 
 
-import '../styles/custom-styles.css';
+
 
 
 import { products } from '../data/products';
@@ -25,7 +25,7 @@ export const ShoppingPage = () => {
           <ProductCard
             product={product}
             key={product.id}
-            className="bg-dark"
+           
             initialValues = {{
                 count:4,
                 maxCount:10
@@ -41,30 +41,25 @@ export const ShoppingPage = () => {
               })=>(
                 <>
                   <ProductImage
-                    className="custom-image"
+                    
                     style={{
                       boxShadow: "10px 10px 10px rgba(0,0,0,,0.2)",
                     }}
                   />
                   <ProductTitle
                     title=""
-                    className="text-white"
+                    
                     style={{ fontWeight: "bold" }}
                   />
                   <ProductButtons
-                    className="custom-buttons"
+                  
                     style={{
                       display: "flex",
                       justifyContent: "end",
                     }}
                   />  
-                  <button onClick={ reset } >Reset</button>      
-                  <button onClick={ ()=>increaseBy(-2) }>-2</button>  
-                  <span>{ count }</span>   
-                  {
-                    !isMountCountReached &&
-                   <button onClick={ ()=>increaseBy(2) }>+2</button>  
-                  }
+                 
+                  
                 </>
               )
             }           
